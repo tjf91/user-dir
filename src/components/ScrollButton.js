45676251feef3b2b1f132634b7ed4ef8537faa3b
tjf91 <tudorjflorea@gmail.com>
2020-11-19 19:04:58 -0700

    forgot to save something?

--- src/components/ScrollButton.js
+++ src/components/ScrollButton.js
@@ -4,11 +4,7 @@ export default function ScrollButton (props){
 
         return(
                 <div >
-                    <button onClick={(index)=>{
-                        if(props.currentIndex+2===props.len){
-                            console.log(props.currentIndex,props.len)
-                        }
-                        return props.handleChangeIndex(props.currentIndex+props.direction)}} className="scrollbutton">{props.carrot}</button>
+                    <button onClick={(index)=>props.handleChangeIndex(props.currentIndex+props.direction)} className="scrollbutton">{props.carrot}</button>
                 </div>
             )
 
